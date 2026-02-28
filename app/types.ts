2711109266a1be2d820af5b23989abb91ba64abd
Iván Rodrigo Ferrioli <ivan.rf.96@gmail.com>
2026-02-28 19:52:55 -0300

    add movieDetail & OmdbRating for Detail API

--- app/types.ts
+++ app/types.ts
@@ -9,3 +9,39 @@ export type MovieSearchResult = {
 export type OmdbSearchResponse =
   | { Response: "True"; Search: MovieSearchResult[]; totalResults: string }
   | { Response: "False"; Error: string };
+
+export type OmdbRating = {
+  Source: string;
+  Value: string;
+};
+
+export type MovieDetail = {
+  Title: string;
+  Year: string;
+  Rated: string;
+  Released: string;
+  Runtime: string;
+  Genre: string;
+  Director: string;
+  Writer: string;
+  Actors: string;
+  Plot: string;
+  Language: string;
+  Country: string;
+  Awards: string;
+  Poster: string;
+  Ratings?: OmdbRating[];
+  Metascore?: string;
+  imdbRating: string;
+  imdbVotes: string;
+  imdbID: string;
+  Type: string;
+  DVD?: string;
+  BoxOffice?: string;
+  Production?: string;
+  Website?: string;
+};
+
+export type OmdbDetailResponse =
+  | (MovieDetail & { Response: "True" })
+  | { Response: "False"; Error: string };
